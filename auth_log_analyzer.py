5738683e44dff438c1860714e98666a2b3be535d
Shashank <sssmoon29@gmail.com>
2025-12-13 22:22:16 +0530

    Add failed authentication detection login

--- auth_log_analyzer.py
+++ auth_log_analyzer.py
@@ -1,3 +1,9 @@
 log_file = "/var/log/auth.log"
-print("Auth Log Analyzer initialized")
+failed_count = 0
 
+with open(log_file, "r")as file:
+  for line in file:
+    if "Failed passowrd" in line or "authentication failure" in line:
+      failed_count += 1
+
+print(f"Total failed authentication attempts: {failed_count}")
